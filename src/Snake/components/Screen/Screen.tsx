f4da620cee06a145d00a2b9fe4f10a53293f04ba
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2020-09-12 15:34:13 -0700

    chore: create new folder structure

--- src/Snake/components/Screen/Screen.tsx
+++ src/Snake/components/Screen/Screen.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { GameState, GridSize } from '@app-src/GameController'
+import { GameState, GridSize } from '@app-snake/GameController'
 
 import styles from './Screen.module.css'
 
