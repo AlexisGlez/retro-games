be947ce8d93d98b76889662884f3991ce92c0969
Alexis González Gómez <alexis.gonzalez.2995@gmail.com>
2020-10-25 14:49:52 -0700

    feat: improve home page (#30)

--- src/App/pages/Home/Home.tsx
+++ src/App/pages/Home/Home.tsx
@@ -8,26 +8,35 @@ import { GamePreview } from './components/GamePreview'
 import { theme } from '@app-shared/theme'
 import { constants } from '@app-shared/constants'
 
+import styles from './Home.module.css'
+
 export const Home: React.FC<HomeData> = ({ games }) => {
   return (
-    <Flex justify="center" align="center" h="100vh" direction="column">
-      <Heading
-        fontSize="6xl"
-        fontFamily="cursive"
-        textAlign="center"
-        fontWeight={theme.fontWeights.extrabold}
-      >
-        Retro Games
-      </Heading>
-      <Text my="1rem" fontSize="3xl" textAlign="center">
-        What do you want to play?
-      </Text>
-      <SimpleGrid p="1rem" columns={{ sm: 1, md: 2, lg: 3 }} spacing="1rem" gridAutoRows="1fr">
-        {Object.keys(games).map((game) => (
-          <GamePreview key={game} {...games[game as GameNames]} />
-        ))}
-      </SimpleGrid>
-    </Flex>
+    <>
+      <div className={styles.fixed}>
+        <div className={styles.cover}>
+          <div className={styles.layer} />
+        </div>
+      </div>
+      <Flex justify="center" align="center" h="100vh" direction="column">
+        <Heading
+          fontSize="6xl"
+          fontFamily="cursive"
+          textAlign="center"
+          fontWeight={theme.fontWeights.extrabold}
+        >
+          Retro Games
+        </Heading>
+        <Text my="1rem" fontSize="3xl" textAlign="center">
+          What do you want to play?
+        </Text>
+        <SimpleGrid p="1rem" columns={{ sm: 1, md: 2, lg: 3 }} spacing="1rem" gridAutoRows="1fr">
+          {Object.keys(games).map((game) => (
+            <GamePreview key={game} {...games[game as GameNames]} />
+          ))}
+        </SimpleGrid>
+      </Flex>
+    </>
   )
 }
 
